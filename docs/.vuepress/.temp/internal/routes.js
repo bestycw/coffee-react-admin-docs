fd8f81f60d624953d71126c2d89d6108fe802c21
yangcw <yangcw@glodon.com>
2024-12-08 23:31:10 +0800

    feat：初始化

---
+++ docs/.vuepress/.temp/internal/routes.js
@@ -0,0 +1,77 @@
+export const redirects = JSON.parse("{\"/docs/guide/permission.html\":\"/docs/guide/permission\",\"/docs/other/question.html\":\"/docs/question/common\",\"/docs/sponsor/sponsor.html\":\"/docs/sponsor/sponsor\"}")
+
+export const routes = Object.fromEntries([
+  ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"/Users/bestycw/Desktop/private/react-admin/react-admin-antd/react-admin-docs/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
+  ["/docs/guide/permission", { loader: () => import(/* webpackChunkName: "docs_guide_permission.html" */"/Users/bestycw/Desktop/private/react-admin/react-admin-antd/react-admin-docs/docs/.vuepress/.temp/pages/docs/guide/permission.html.js"), meta: {"title":"权限管理"} }],
+  ["/docs/question/common", { loader: () => import(/* webpackChunkName: "docs_question_common.html" */"/Users/bestycw/Desktop/private/react-admin/react-admin-antd/react-admin-docs/docs/.vuepress/.temp/pages/docs/question/common.html.js"), meta: {"title":"常见问题"} }],
+  ["/docs/sponsor/sponsor", { loader: () => import(/* webpackChunkName: "docs_sponsor_sponsor.html" */"/Users/bestycw/Desktop/private/react-admin/react-admin-antd/react-admin-docs/docs/.vuepress/.temp/pages/docs/sponsor/sponsor.html.js"), meta: {"title":"赞助与合作"} }],
+]);
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateRoutes) {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+  }
+  if (__VUE_HMR_RUNTIME__.updateRedirects) {
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ routes, redirects }) => {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  })
+}
+.html" */"/Users/bestycw/Desktop/private/react-admin/react-admin-antd/react-admin-docs/docs/.vuepress/.temp/pages/docs/sponsor/sponsor.html.js"), meta: {"title":"赞助与合作"} }],
+]);
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateRoutes) {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+  }
+  if (__VUE_HMR_RUNTIME__.updateRedirects) {
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ routes, redirects }) => {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  })
+}
+onsor_sponsor.html" */"/Users/bestycw/Desktop/private/react-admin/react-admin-antd/react-admin-docs/docs/.vuepress/.temp/pages/docs/sponsor/sponsor.html.js"), meta: {"title":"赞助与合作"} }],
+]);
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateRoutes) {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+  }
+  if (__VUE_HMR_RUNTIME__.updateRedirects) {
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ routes, redirects }) => {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  })
+}
+utes) {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+  }
+  if (__VUE_HMR_RUNTIME__.updateRedirects) {
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ routes, redirects }) => {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  })
+}
